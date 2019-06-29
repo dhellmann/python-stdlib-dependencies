fb24b528a9ac9b3261b0d449dbfa91b160e9c221
Doug Hellmann <doug@doughellmann.com>
2019-06-29 15:39:06 -0400

    generate dot output
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- graph.py
+++ graph.py
@@ -14,6 +14,10 @@ class Scanner:
         self._errors = {}
         self._seen = set()
 
+    @property
+    def module_names(self):
+        return list(self._imports.keys())
+
     def _get_imports(self, filename):
         if filename in self._seen:
             return
@@ -73,9 +77,48 @@ class Scanner:
             imported_name = self._get_module_name_from_import(imported)
             self.show_module(imported_name, depth+1, shown)
 
+    def get_edges(self, name, shown=None):
+        # Avoid recursing after we have shown the dependencies of a
+        # module once.
+        if shown is None:
+            shown = set()
+        if name in shown:
+            return
+        shown.add(name)
+
+        for imported in self._imports[name]:
+            imported_name = self._get_module_name_from_import(imported)
+            target_module = self._get_module_name_from_import(imported)
+            yield (name, target_module)
+            yield from self.get_edges(target_module, shown)
+
+
+def get_dot_graph(scanner, module_name):
+    yield 'digraph stdlib {'
+
+    # All of the modules in the scanner as nodes
+    for used_module in scanner.module_names:
+        yield '  "{}" [ shape=oval ]'.format(used_module)
+
+    yield ''
+
+    known_edges = set()
+
+    # Edges for imports from this module
+    for start, end in scanner.get_edges(module_name):
+        uniq_edge = (start, end)
+        if uniq_edge in known_edges:
+            continue
+        known_edges.add(uniq_edge)
+        yield '  "{}" -> "{}"'.format(start, end)
+
+    yield ''
+    yield '}'
+
 
 if __name__ == '__main__':
     import urllib.request
     scanner = Scanner()
     scanner.scan_module(urllib.request)
-    scanner.show_module('urllib.request')
+    for line in get_dot_graph(scanner, 'urllib.request'):
+        print(line)
